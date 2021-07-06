2096bb69989d9b82a54f61e28035051584f6ae6a
Aldrin Feliciano <aldrenaline369@gmail.com>
2021-07-06 16:36:59 -0600

    add remove function

--- document.py
+++ document.py
@@ -33,6 +33,11 @@ class Document:
            "\x00(Feedback((doc_id 0)(span_id 1)(route 0)(contents Processed)))\x00")
     
     def isConsistent(self):
+        ''' checks that the coq document and member variables are consistent '''
+
+        # list of keys should be the same as the range of sid's
+        if list(self.contents.keys()) != list(range(2, self.totalLines + 1)):
+            return False
 
         # every line upto self.currentLine should be executed
         for sid in range(2, self.currentLine + 1):
@@ -51,33 +56,37 @@ class Document:
         
         # lines past self.totalLines should not exist, leading to "uncaught exception"
         sendCommand(self.proc, f"(Query ((sid {self.totalLines + 1}) (pp ((pp_format PpStr)))) Ast)")
-        # TODO: there may be a better way to check for this
+            # TODO: there may be a better way to check for this
         if "Uncaught exception" not in open("log.txt").readlines()[-1]:
             return False
         
         # compare strings of each line
-        '''
         for sid in range(2, self.totalLines + 1):
             sendCommand(self.proc, f"(Query ((sid {sid}) (pp ((pp_format PpStr)))) Ast)")
             responseStr = open("log.txt").readlines()[-1]
-            coqStr = responseStr.partition("CoqString")[2].partition("))))")[0]
-            print(coqStr)
-            print(self.contents[sid].statement)
-            print("\n")
-        '''
-            # TODO: not sure how to compare the strings, discrepancy in parenthesis
+            coqStr = responseStr.partition("CoqString\"")[2].partition("\"))))")[0]
+            if (coqStr != self.contents[sid].statement):
+                return False
 
         return True
-
   
     def addStatement(self, coqStr):
+        ''' adds the corresponding coqStr to the document, only one line at a time '''
+
+        self.totalLines += 1
+
         commandString = "(Add () \"" + coqStr + "\")"
         sendCommand(self.proc, commandString)
 
-        self.totalLines += 1
+        # change string to match what is stored by Coq
+        sendCommand(self.proc, f"(Query ((sid {self.totalLines}) (pp ((pp_format PpStr)))) Ast)")
+        coqStr = open("log.txt").readlines()[-1]
+        coqStr = coqStr.partition("CoqString\"")[2].partition("\"))))")[0]
+
         self.contents[self.totalLines] = self.Line(coqStr)
     
     def executeAndQueryGoals(self, sid):
+        ''' executes up to the specified sid, and returns goals at that sid '''
         sendCommand(self.proc, f"(Exec {sid})")
         sendCommand(self.proc, f"(Query ((sid {sid}) (pp ((pp_format PpStr)))) Goals)")
         parseAndPrintGoals(open("log.txt").readlines()[-1])
@@ -87,6 +96,19 @@ class Document:
         
         if self.currentLine < sid:
             self.currentLine = sid
+
+    def removeStatement(self):
+        ''' removes the last statement of the document, returns the line removed as a string '''
+        removedStr = self.contents[self.totalLines].statement
+        
+        sendCommand(self.proc, f"(Cancel ({self.totalLines}))")
+        self.contents.pop(self.totalLines)
+
+        self.totalLines -= 1
+        if self.currentLine > self.totalLines:
+            self.currentLine -= 1
+
+        return removedStr
 
     
     class Line:
@@ -98,9 +120,9 @@ class Document:
 
 def testing():
     doc = Document()
-    print(doc.isConsistent())
+    print(doc.isConsistent(), "consistency")
 
-    doc.addStatement("From Defs Require Export group_theory.")
+    doc.addStatement("From Defs Require Export group_theory.") # starts here with sid 2
     doc.addStatement("Lemma boolean_implies_abelian : (forall x : group_theory.G, x<*>x = e) -> (is_abelian group_theory.G).")
     doc.addStatement("Proof.")
     doc.addStatement("intros.")
@@ -114,22 +136,16 @@ def testing():
     print("Current total lines is: ", doc.totalLines)
     print("Current executed lines is:", doc.currentLine)
 
-    print(doc.isConsistent())
+    doc.executeAndQueryGoals(9)
+    print("Current total lines is: ", doc.totalLines)
+    print("Current executed lines is:", doc.currentLine)
 
-    '''
-    sendCommand(doc.proc, "(Query ((sid 7) (pp ((pp_format PpStr)))) Goals)")
-    sendCommand(doc.proc, "(Query ((sid 8) (pp ((pp_format PpStr)))) Goals)")
-    sendCommand(doc.proc, "(Exec 8)")
-    sendCommand(doc.proc, "(Query ((sid 8) (pp ((pp_format PpStr)))) Goals)")
-    sendCommand(doc.proc, "(Query ((sid 10) (pp ((pp_format PpStr)))) Ast)")
+    print(doc.isConsistent(), "consistency")
 
-    sendCommand(doc.proc, "(Exec 9)")
-    sendCommand(doc.proc, "(Query ((sid 9) (pp ((pp_format PpStr)))) Ast)")
-    sendCommand(doc.proc, "(Query ((sid 9) (pp ((pp_format PpStr)))) Goals)")
-    sendCommand(doc.proc, "(Exec 7)")
-    sendCommand(doc.proc, "(Query ((sid 9) (pp ((pp_format PpStr)))) Ast)")
-    sendCommand(doc.proc, "(Query ((sid 9) (pp ((pp_format PpStr)))) Goals)")
-    '''
+    doc.removeStatement()
+    doc.removeStatement()
+    print(doc.isConsistent(), "consistency")
+
 
 
 testing()
